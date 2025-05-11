ef052b45c059f48fdb24be3f5abd882d6fe610ee
TMNhat1810 <minhnhat912002@gmail.com>
2025-05-12 03:00:15 +0700

    feat: add user view log

--- src/components/MediaControl/VideoControl/VideoControl.tsx
+++ src/components/MediaControl/VideoControl/VideoControl.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/material';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 interface VideoControlPropsType {
   video_url: string;
@@ -9,11 +10,49 @@ export default function VideoControl({
   video_url,
   thumbnail_url,
 }: VideoControlPropsType) {
+  const mediaRef = useRef<HTMLVideoElement | null>(null);
+  const playtimeRef = useRef<number>(0);
+  const [logged, setLogged] = useState<boolean>(false);
+  const [playing, setPlaying] = useState<boolean>(false);
+
+  const handleLoaded = () => {
+    if (!mediaRef.current) return;
+
+    mediaRef.current.muted = true;
+    mediaRef.current.play().catch();
+  };
+
+  const handleLogUserView = useCallback(() => {
+    if (logged) return;
+    setLogged(true);
+  }, [logged]);
+
+  useEffect(() => {
+    if (!mediaRef.current || !playing) return;
+    if (logged) return;
+
+    const interval = setInterval(() => {
+      playtimeRef.current += 1;
+      if (playtimeRef.current >= 10) {
+        handleLogUserView();
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [playing, logged, handleLogUserView]);
+
   return (
     <Box>
       {thumbnail_url && <></>}
       <video
         controls
+        onLoadedData={handleLoaded}
+        onPlay={() => setPlaying(true)}
+        onPause={() => setPlaying(false)}
+        onEnded={handleLogUserView}
+        ref={mediaRef}
         style={{
           width: '100%',
           accentColor: 'cyan',
